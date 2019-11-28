a1624602b92fad07f62bb7cc7b8c06d19c04eafc
udeogu ifechukwu <ifebrand6@gmail.com>
2019-11-28 18:40:48 +0100

    Setup Application user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :notes
   mount Sidekiq::Web => '/sidekiq' # monitoring console
   root 'notes#index'
