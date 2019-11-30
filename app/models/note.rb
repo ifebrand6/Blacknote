b83ada1e46f7821657748e4bbe112f5e14f00a38
udeogu ifechukwu <ifebrand6@gmail.com>
2019-11-30 19:07:35 +0100

    added relational entity to database

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,5 +3,6 @@
 class Note < ApplicationRecord
     belongs_to :user
     belongs_to :category
+    has_many :tags, class_name: "tag", foreign_key: "reference_id"
     validates :content, length: { minimum: 200}
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,3 @@
 class Tag < ApplicationRecord
+    belongs_to :note, class_name: "note", foreign_key: "note_id"
 end
