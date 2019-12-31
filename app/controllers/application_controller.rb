3ff13cda74768c4f6cf893a08f48ad899f94db37
udeogu ifechukwu <ifebrand6@gmail.com>
2019-12-31 08:47:18 +0100

    added seo meta tags to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,11 @@ class ApplicationController < ActionController::Base
   # @city = request.location.city
   before_action :set_variables
   before_action :set_category
+  before_action :prepare_meta_tags
   layout :layout_by_resource
 
+  
+  end
   def layout_by_resource
     if devise_controller? && user_signed_in?
       'react'
@@ -20,4 +23,11 @@ class ApplicationController < ActionController::Base
   def set_category
     @category = Category.find_or_create_by(id:1, name: "All")
   end
+  def prepare_meta_tags(options={})
+    site_name   = "Blacknote"
+    title       = [controller_name, action_name].join(" ")
+    description = "Black Note is a new way to save memories, the app that will help you beautifully showcase your orignality, creative art or anything else, you can make with words!."
+    image       = options[:image] || "your-default-image-url"
+    current_url = request.url
 end
+
